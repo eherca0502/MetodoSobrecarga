ce8966028ac84e30ec8641629ded0069504a78eb
eherca0502 <eduardoicc@outlook.es>
2023-03-08 22:17:03 -0600

    Agregar archivos de proyecto.

---
+++ School/Escuela.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Escuela
+    {
+        private string nombre;
+        private string direccion;
+
+        public Escuela(string nombre, string direccion)
+        {
+            this.nombre = nombre;
+            this.direccion = direccion;
+        }
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("Nombre de la escuela: {0}", this.nombre);
+            Console.WriteLine("Dirección de la escuela: {0}", this.direccion);
+        }
+        public void MostrarInformacion(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+            Console.WriteLine("Nombre de la escuela: {0}", this.nombre);
+            Console.WriteLine("Dirección de la escuela: {0}", this.direccion);
+        }
+        public void MostrarInformacion(string mensaje, bool incluirDireccion)
+        {
+            Console.WriteLine(mensaje);
+            Console.WriteLine("Nombre de la escuela: {0}", this.nombre);
+
+            if (incluirDireccion)
+            {
+                Console.WriteLine("Dirección de la escuela: {0}", this.direccion);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ School/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Escuela miEscuela = new Escuela("Mi escuela", "Calle principal 123");
+
+            miEscuela.MostrarInformacion();
+            Console.WriteLine();
+            miEscuela.MostrarInformacion("Información de la escuela:");
+            Console.WriteLine();
+            miEscuela.MostrarInformacion("Información de la escuela:", true);
+
+            Console.ReadKey();
+        }
+    }
+}
